b56bc144b44c1e19b09f73651ebc71e94b9c23f4
Ernests <austrins.ernests@gmail.com>
2025-04-15 16:06:23 +0300

    fixed an error with params

--- src/app/tasks/[id]/page.tsx
+++ src/app/tasks/[id]/page.tsx
@@ -1,6 +1,7 @@
 import TaskDetails from "@/components/task-details";
 
-export default function TaskDetailsPage({ params }: { params: { id: string } }) {
+export default async function TaskDetailsPage(props: { params: Promise<{ id: string }> }) {
+  const params = await props.params;
   return (
     <div className="w-full max-w-lg">
       <TaskDetails id={params.id} />
