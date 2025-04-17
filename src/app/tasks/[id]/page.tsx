a0881207a7d0e3eeee5a872d5a7d8e14cf8a0fae
Ernests <austrins.ernests@gmail.com>
2025-04-17 13:05:15 +0300

    fixed error with task editing params
    Added @next/codemod@canary next-async-request-api .

--- src/app/tasks/[id]/page.tsx
+++ src/app/tasks/[id]/page.tsx
@@ -1,6 +1,10 @@
 import TaskDetails from "@/components/task-details";
 
-export default async function TaskDetailsPage({params, searchParams}: {params: {id: string}, searchParams: {edit?: string}}) {
+export default async function TaskDetailsPage(
+  props: {params: Promise<{id: string}>, searchParams: Promise<{edit?: string}>}
+) {
+  const searchParams = await props.searchParams;
+  const params = await props.params;
   return (
     <div className="w-full max-w-lg">
       <TaskDetails id={params.id} searchParams={searchParams} />
